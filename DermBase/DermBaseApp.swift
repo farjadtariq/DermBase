a84124d3e02061fa0a4c922bbff15116e3bd6a8a
farjadtariq <farjadtariq@gmail.com>
2023-04-02 07:13:31 +0500

    Initial Commit

---
+++ DermBase/DermBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DermBaseApp.swift
+//  DermBase
+//
+//  Created by Farjad on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DermBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
