2915351e072f607b3c01c86ff95377da8ffffbee
Farjad Tariq <49763743+farjadtariq@users.noreply.github.com>
2023-04-02 21:57:47 +0500

    Start

--- DermBase/ContentView.swift
+++ DermBase/ContentView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(hex: "1C3968")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Image("dermbase_logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .padding(.bottom, 40)
+                Text("Welcome to DermBase")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 20)
+                Button(action: {
+                    // handle button action
+                }) {
+                    Text("Get Started")
+                        .foregroundColor(Color(hex: "1C3968"))
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .padding()
+                }
+                .background(Color(hex: "E37825"))
+                .cornerRadius(10)
+                .padding(.horizontal, 50)
+                Spacer()
+            }
         }
-        .padding()
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        self.init(
+            .sRGB,
+            red: Double((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: Double((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: Double(rgbValue & 0x0000FF) / 255.0,
+            opacity: 1.0
+        )
     }
 }
 
